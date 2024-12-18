2a8287e9ada2b93b7d112355b91ed419839358cf
Bora Sevim <sevimbora@gmail.com>
2024-12-18 21:16:39 +0300

    pedometer always runs in background

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:walkverse/chart.dart';
 import 'package:provider/provider.dart';
 import 'package:walkverse/login.dart';
+import 'package:walkverse/pedometer_service.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
+  await Firebase.initializeApp();
+  await requestPermissions(); // Request necessary permissions
+  await pedometer_service().initializeService(); // Initialize the singleton service
+
   runApp(MultiProvider(
     providers: [ChangeNotifierProvider(create: (_) => ChartDataProvider())],
     child: const MyApp(),
   ));
+}
+
+Future<void> requestPermissions() async {
+  await [
+    Permission.activityRecognition,
+    Permission.locationWhenInUse,
+    Permission.locationAlways,
+  ].request();
 }
 
 class MyApp extends StatelessWidget {
