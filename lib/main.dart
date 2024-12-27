a09ee817829eb86e878816f027fcfe4cfdf1ea2a
Fleqpe <bariskurt2002@hotmail.com>
2024-12-27 10:23:07 +0300

    Adımlar

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:walkverse/chart.dart';
 import 'package:provider/provider.dart';
 import 'package:walkverse/login.dart';
 import 'package:walkverse/PedometerService.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
@@ -11,10 +12,21 @@ void main() async {
   await PedometerService()
       .initializeService(); // Initialize the singleton service
 
+  // Gerekli izinleri iste
+  await _requestPermissions();
+
   runApp(MultiProvider(
     providers: [ChangeNotifierProvider(create: (_) => ChartDataProvider())],
     child: const MyApp(),
   ));
+}
+
+Future<void> _requestPermissions() async {
+  await [
+    Permission.activityRecognition,
+    Permission.location,
+    Permission.storage,
+  ].request();
 }
 
 class MyApp extends StatelessWidget {
