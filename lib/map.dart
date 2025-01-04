0ebec0d30ea3ade7f607ddfc5db3e343d7d8e89b
knyeow <112291638+knyeow@users.noreply.github.com>
2025-01-04 20:29:11 +0300

    Revert "fixes"
    This reverts commit 92ce7ca0a0dbcb2c68d970fcca957c0c7ec0dc8c.

--- lib/map.dart
+++ lib/map.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-class Map extends StatelessWidget {
-  const Map({super.key});
+class Map extends StatelessWidget
+{
+  const Map({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return Column();
   }
 }
