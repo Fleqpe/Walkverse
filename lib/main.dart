5f204b87e1414ef0c75ec99cf41e14d02903b1e1
knyeow <112291638+knyeow@users.noreply.github.com>
2025-01-04 20:29:07 +0300

    Revert "Adımlar"
    This reverts commit a09ee817829eb86e878816f027fcfe4cfdf1ea2a.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:walkverse/chart.dart';
 import 'package:provider/provider.dart';
 import 'package:walkverse/login.dart';
 import 'package:walkverse/PedometerService.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
@@ -12,21 +11,10 @@ void main() async {
   await PedometerService()
       .initializeService(); // Initialize the singleton service
 
-  // Gerekli izinleri iste
-  await _requestPermissions();
-
   runApp(MultiProvider(
     providers: [ChangeNotifierProvider(create: (_) => ChartDataProvider())],
     child: const MyApp(),
   ));
-}
-
-Future<void> _requestPermissions() async {
-  await [
-    Permission.activityRecognition,
-    Permission.location,
-    Permission.storage,
-  ].request();
 }
 
 class MyApp extends StatelessWidget {
